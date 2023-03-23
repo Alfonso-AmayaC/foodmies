d586fcd5aea123695b37a054d38cc00b6e098232
Alfonso Amaya <alfonsocamargo0108@gmail.com>
2023-03-23 09:12:24 -0600

    Create first version

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,32 +1,33 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import { Grid, GridItem, Heading, Stack } from '@chakra-ui/react';
+import Head from 'next/head';
+import Link from 'next/link';
 
-type Props = {
-  children?: ReactNode
-  title?: string
+export default function Layout({ children, path }) {
+    
+    return (
+        <>
+            <Head>
+                <link rel='icon' href='/favicon.png' />
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+            <Grid h={'100vh'} templateRows={'repeat(2, 1fr)'} templateColumns={'repeat(6, 1fr)'} position={'relative'}>
+                <GridItem rowSpan={2} colSpan={1} bg={'#BFE4FF'} roundedBottomRight={'10px'} roundedTopRight={'10px'} zIndex={'modal'} roundedRight={'20px'} borderRight={'9px solid #E2E8F0'}>
+                <Stack direction={'column'} gap={10} pt={'10'}>
+                    <Link href={'/pantry'} className={'menuLogo'}>
+                        <Heading as={'h2'} fontFamily={'Permanent Marker'} textAlign={'center'}>Foodmies</Heading>
+                    </Link>
+                    <Stack>
+                        <Link href={'/pantry'} className={`menuItem ${path === 'pantry' ? 'active' : ''}`}>Despensa</Link>
+                        <Link href={'/meals'} className={`menuItem ${path === 'meals' ? 'active' : ''}`}>Comidas planeadas</Link>
+                        <Link href={'/history'} className={`menuItem ${path === 'history' ? 'active' : ''}`}>Histórico de precios</Link>
+                    </Stack>
+                </Stack>
+                </GridItem>
+                <GridItem rowSpan={2} colSpan={5} bg='#FFFFFF' pt={'10'} px={'14'} fontFamily={'inter'} overflowY={'scroll'}>
+                    {children}
+                </GridItem>
+            </Grid>
+        </>
+    )
 }
-
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
-        <Link href="/users">Users List</Link> |{' '}
-        <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
-
-export default Layout
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,18 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Head from 'next/head';
+import { Box, Heading } from '@chakra-ui/react';
+import Login from '../components/login';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Foodmies - Log In</title>
+      </Head>
+      <Box bg={'yellow'} w={'100vw'} height={'100vh'}>
+          <Login />
+      </Box>
+    </>
+  )
+}
 
 export default IndexPage
